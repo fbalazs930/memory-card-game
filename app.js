d57abf6062567cec34017eda23934304a400ddee
fbalazs930 <fbalazs930@gmail.com>
2022-04-22 15:07:13 +0200

    adding logic

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ randomize();
 const cardGenerator = () => {
     const cardData = randomize();
 
-    cardData.forEach((item) => {
+    cardData.forEach((item, index) => {
         const card = document.createElement("div");
         const face = document.createElement("img");
         const back = document.createElement("div");
@@ -45,16 +45,31 @@ const cardGenerator = () => {
         back.className = 'back';
 
         face.src = item.imgSrc;
-
+        card.setAttribute('name', item.name);
         section.appendChild(card);
         card.appendChild(face);
         card.appendChild(back);
 
-        card.addEventListener('click', () => {
+        card.addEventListener('click', (e) => {
             card.classList.toggle('toggleCard');
+            checkCards(e);
         });
     })
 }
 
+const checkCards = (e) => {
+    const clickedCard = e.target;
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped');
+
+    if (flippedCards.length === 2) {
+        if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
+            console.log("match")
+        }
+        else{
+            console.log("not match")
+        }
+    }
+}
 
 cardGenerator();
