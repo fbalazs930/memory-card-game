5485c86610423f1bd73846d81745419548d5240f
fbalazs930 <fbalazs930@gmail.com>
2022-04-22 14:22:39 +0200

    data added, some styles

---
+++ app.js
@@ -0,0 +1,56 @@
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('.playerLivesCount');
+const playerLives = 6;
+
+playerLivesCount.textContent = playerLives;
+
+
+const getData = () => [
+    { imgSrc: "./images/beatles.jpeg", id: 1, name: "beatles" },
+    { imgSrc: "./images/blink182.jpeg", id: 2, name: "blink 182" },
+    { imgSrc: "./images/fkatwigs.jpeg", id: 3, name: "fka twigs" },
+    { imgSrc: "./images/fleetwood.jpeg", id: 4, name: "fleetwood" },
+    { imgSrc: "./images/joy-division.jpeg", id: 5, name: "joy division" },
+    { imgSrc: "./images/ledzep.jpeg", id: 6, name: "lep zeppelin" },
+    { imgSrc: "./images/metallica.jpeg", id: 7, name: "metallica" },
+    { imgSrc: "./images/pinkfloyd.jpeg", id: 8, name: "pink floyd" },
+    { imgSrc: "./images/beatles.jpeg", id: 9, name: "beatles" },
+    { imgSrc: "./images/blink182.jpeg", id: 10, name: "blink 182" },
+    { imgSrc: "./images/fkatwigs.jpeg", id: 11, name: "fka twigs" },
+    { imgSrc: "./images/fleetwood.jpeg", id: 12, name: "fleetwood" },
+    { imgSrc: "./images/joy-division.jpeg", id: 13, name: "joy division" },
+    { imgSrc: "./images/ledzep.jpeg", id: 14, name: "led zeppelin" },
+    { imgSrc: "./images/metallica.jpeg", id: 15, name: "metallica" },
+    { imgSrc: "./images/pinkfloyd.jpeg", id: 16, name: "pink floyd" },
+]
+
+
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+}
+
+randomize();
+
+const cardGenerator = () => {
+    const cardData = randomize();
+
+    cardData.forEach((item) => {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.className = 'card';
+        face.className = 'face';
+        back.className = 'back';
+
+        face.src = item.imgSrc;
+
+        card.appendChild(face);
+        card.appendChild(back);
+        section.appendChild(card);
+    })
+}
+
+
+cardGenerator();
